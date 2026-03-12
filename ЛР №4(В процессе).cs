44f301b569f87032bdab0470c58b930c6bae3c6e
krukva <krukva337@gmail.com>
2026-03-12 11:38:32 +0500

    Add files via upload

---
+++ ЛР №4(В процессе).cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ЛР__3
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Вывод всех процессов:");
+            foreach (Process process in Process.GetProcesses())
+            {
+                Console.WriteLine($"Id: {process.Id} Name: {process.ProcessName}");
+            }
+
+
+            Console.WriteLine("\n=================================================================");
+            Console.WriteLine("Вывод всех потоков процесса Visual Studio:");
+            Process proc = Process.GetProcessesByName("devenv")[0];
+            ProcessThreadCollection processThread = proc.Threads;
+            foreach (ProcessThread thread in processThread)
+            {
+                Console.WriteLine($"ThreadId: {thread.Id}");
+            }
+
+
+            Console.WriteLine("\n=================================================================");
+            Console.WriteLine("Запус нескольких сторонних процессов:");
+            Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome");
+            Process.Start(@"C:\Program Files\Microsoft Office\Office16\EXCEL");
+            Process.Start("mspaint.exe");
+            Console.WriteLine("Chrome\nExcel\nPaint");
+        }
+    }
+}
