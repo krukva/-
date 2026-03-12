b2f9b09e0674dc979ef7d97485ab38fd06908450
krukva <krukva337@gmail.com>
2026-03-12 09:52:16 +0500

    Add files via upload

---
+++ ЛР №3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace ЛР__3
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            Application excelApp = new Application();
+
+            if (excelApp == null);
+            {
+                Console.WriteLine("Excel не установлен");
+                return;
+            }
+
+            Workbook excelBook = excelApp.Workbooks.Open(@"E:\readExampel.xlsx");
+
+            _Worksheet excelSheet = excelBook.Sheets[1];
+            Range excelRange = excelSheet.UsedRange;
+
+            int rows = excelRange.Rows.Count;
+            int cols = excelRange.Columns.Count;
+
+            for (int i = 1; i <= rows; i++)
+            {
+                Console.Write("\r\n");
+                for (int j = 2; j <= cols; j++)
+                {
+                    if (excelRange.Cells[i, j] != null && excelRange.Cells[i, j].Value2 != null)
+                        Console.Write(excelRange.Cells[i, j].Value2.ToString() + "\t");
+                }
+            }
+            excelApp.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+            Console.ReadLine();
+        }
+    }
+}
